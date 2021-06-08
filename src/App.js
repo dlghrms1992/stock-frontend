c4a5d4c00e253eed7da11a6cd223abf920504c6f
dlghrms1992 <dlghrms1992@naver.com>
2021-06-08 22:44:16 +0900

    index header 시작

--- src/App.js
+++ src/App.js
@@ -2,49 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 import customAxios from './customAxios';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Router,
-  Link,
-  Route
-} from 'react-router-dom'
 
-function App(){
-  return(
-    <Router>
-      <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">홈</Link>
-            </li>
-            <li>
-              <Link to="/about">소개</Link>
-            </li>
-            <li>
-              <Link to="/users">사용자</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+// function App(){
+//   return(
+//     <Router>
+//       <div className="App">
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/">홈</Link>
+//             </li>
+//             <li>
+//               <Link to="/about">소개</Link>
+//             </li>
+//             <li>
+//               <Link to="/users">사용자</Link>
+//             </li>
+//           </ul>
+//         </nav>
+//       </div>
 
-      <Switch>
-        <Route path="/about">
-          <About/>
-        </Route>
-        <Route path="/users">
-          <Users/>
-        </Route>
-        <Route path="/">
-          <Home/>
-        </Route>
-      </Switch>
-    </Router>
-  );
-}
+//       <Switch>
+//         <Route path="/about">
+//           <About/>
+//         </Route>
+//         <Route path="/users">
+//           <Users/>
+//         </Route>
+//         <Route path="/">
+//           <Home/>
+//         </Route>
+//       </Switch>
+//     </Router>
+//   );
+// }
 
-function Home() {
+function App() {
   const [ip, setIp] = useState('');
   
   function callback(data){
@@ -65,21 +58,21 @@ function Home() {
     </div>
   );
 }
-function About() {
-  return (
-    <div>
-      <hr />
-      <h2>소개 페이지</h2>
-    </div>
-  );
-}
+// function About() {
+//   return (
+//     <div>
+//       <hr />
+//       <h2>소개 페이지</h2>
+//     </div>
+//   );
+// }
 
-function Users() {
-  return (
-    <div>
-      <hr />
-      <h2>사용자 페이지</h2>
-    </div>
-  );
+// function Users() {
+//   return (
+//     <div>
+//       <hr />
+//       <h2>사용자 페이지</h2>
+//     </div>
+//   );
 }
 export default App;
